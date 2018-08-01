0cef1322832d9351d3a75e9de542654881ac0e09
sakura <975976959@qq.com>
2018-08-01 18:17:13 +0800

    upup

--- app/src/main/java/com/sakuraprint2/Activity/MainActivity.java
+++ app/src/main/java/com/sakuraprint2/Activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.sakuraprint2.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
@@ -24,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
                 getSupportFragmentManager(), FragmentPagerItems.with(this)
                 .add(R.string.a1, MingXiFragment.class)
@@ -34,10 +32,8 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.string.a6, ZhangDanFragment.class)
                 .add(R.string.a7, RiZhiFragment.class)
                 .create());
-
         viewpager.setAdapter(adapter);
         viewpagertab.setViewPager(viewpager);
-
     }
 
     private void initView() {
